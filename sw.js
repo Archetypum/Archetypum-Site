8b6a2c2929f6f6c2429c6f5468daed9051435b39
WretchOfLights <TotallyNotDuhMakaron@proton.me>
2025-10-04 02:53:14 +0600

    Create sw.js
    shit fuck

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "black-site-v1";
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css'
+];
+
+// Install event
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
